b2e1a977da4d746ac7806086559f1f302c0180a3
AndreasLofberg1 <andreas.lofberg@contentowassum.se>
2018-08-31 15:39:25 +0200

    First Commit

---
+++ SeedData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace AspNetCoreTodo
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(
+        IServiceProvider services)
+        {
+            var roleManager = services
+            .GetRequiredService<RoleManager<IdentityRole>>();
+            await EnsureRolesAsync(roleManager);
+            var userManager = services
+            .GetRequiredService<UserManager<IdentityUser>>(
+            );
+            await EnsureTestAdminAsync(userManager);
+        }
+    }
+}
---
+++ ViewModels/ManageUsersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.ViewModels
+{
+    public class ManageUsersViewModel
+    {
+        public IdentityUser[] Administrators { get; set; }
+        public IdentityUser[] EveryoneElse { get; set; }
+        public IdentityUser[] Everyone { get; set; }
+        
+    }
+}
